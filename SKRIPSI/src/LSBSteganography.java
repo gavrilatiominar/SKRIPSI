3f2c90cfd8be45a88ffb4de9443164bd9ea283ab
Gavrila <ts.gavrila@gmail.com>
2018-02-06 15:43:39 +0700

    6 feb 18 5x5 masih gak bisa

--- SKRIPSI/src/LSBSteganography.java
+++ SKRIPSI/src/LSBSteganography.java
@@ -23,39 +23,73 @@ public class LSBSteganography extends Steganography {
         String scrt = this.secretDataToBinary();
         int x = 0;
         int y = 0;
+        int[] newrgb = new int[3];
         for (int i = 0; i < scrt.length(); i++) {
+            int r = this.coverImage.getRedValue(x, y);
+            int g = this.coverImage.getGreenValue(x, y);
+            int b = this.coverImage.getBlueValue(x, y);
             if (i % 3 == 0) {
-                int r = this.coverImage.getRedValue(x, y);
                 if (r % 2 == 0) {
                     if (scrt.charAt(i) == '1') {
-                        this.coverImage.setRedValue(r + 1, x, y);
+                        //this.coverImage.setRedValue(r + 1, x, y);
+                        newrgb[0] = r + 1;
+                    } else {
+                        newrgb[0] = r;
                     }
                 } else {
                     if (scrt.charAt(i) == '0') {
-                        this.coverImage.setRedValue(r - 1, x, y);
+                        //this.coverImage.setRedValue(r - 1, x, y);
+                        newrgb[0] = r - 1;
+                    } else {
+                        newrgb[0] = r;
                     }
+                }
+                try {
+                    this.coverImage.setPixelValue(x, y, newrgb[0], g, b);
+                } catch (IOException ex) {
+                    Logger.getLogger(LSBSteganography.class.getName()).log(Level.SEVERE, null, ex);
                 }
             } else if (i % 3 == 1) {
-                int g = this.coverImage.getGreenValue(x, y);
                 if (g % 2 == 0) {
                     if (scrt.charAt(i) == '1') {
-                        this.coverImage.setGreenValue(g + 1, x, y);
+                        //this.coverImage.setGreenValue(g + 1, x, y);
+                        newrgb[1] = g + 1;
+                    } else {
+                        newrgb[1] = g;
                     }
                 } else {
                     if (scrt.charAt(i) == '0') {
-                        this.coverImage.setGreenValue(g - 1, x, y);
+                        //this.coverImage.setGreenValue(g - 1, x, y);
+                        newrgb[1] = g - 1;
+                    } else {
+                        newrgb[1] = g;
                     }
                 }
+                try {
+                    this.coverImage.setPixelValue(x, y, newrgb[0], newrgb[1], b);
+                } catch (IOException ex) {
+                    Logger.getLogger(LSBSteganography.class.getName()).log(Level.SEVERE, null, ex);
+                }
             } else {
-                int b = this.coverImage.getBlueValue(x, y);
                 if (b % 2 == 0) {
                     if (scrt.charAt(i) == '1') {
-                        this.coverImage.setBlueValue(b + 1, x, y);
+                        //this.coverImage.setBlueValue(b + 1, x, y);
+                        newrgb[2] = b + 1;
+                    } else {
+                        newrgb[2] = b;
                     }
                 } else {
                     if (scrt.charAt(i) == '0') {
-                        this.coverImage.setBlueValue(b - 1, x, y);
+                        //this.coverImage.setBlueValue(b - 1, x, y);
+                        newrgb[2] = b - 1;
+                    } else {
+                        newrgb[2] = b;
                     }
+                }
+                try {
+                    this.coverImage.setPixelValue(x, y, newrgb[0], newrgb[1], newrgb[2]);
+                } catch (IOException ex) {
+                    Logger.getLogger(LSBSteganography.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 if (x < this.coverImage.getImgWidth()) {
                     x++;
