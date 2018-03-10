bd97a7fa0c1a80f42fb84b35035144d8f74de580
Gavrila <ts.gavrila@gmail.com>
2018-03-11 00:32:09 +0700

    chi

--- SKRIPSI/src/ChiSquare.java
+++ SKRIPSI/src/ChiSquare.java
@@ -15,19 +15,32 @@ public class ChiSquare {
 
     ImageProcessor ip;
     int[][] pixelVal;
-    long[] X;
-    double[] Y;
-    double[] Z;
+    long[] XR;
+    long[] XG;
+    long[] XB;
+    double[] YR;
+    double[] YG;
+    double[] YB;
+    double[] ZR;
+    double[] ZG;
+    double[] ZB;
     double[] chiSquared;
     double probabilityRate;
-    int chuckSize = 128;
+    int chunkSize = 128;
     int numOfChunks = 0;
     ChiSquareTest chi;
 
     public ChiSquare(String filePath) throws IOException {
         this.ip = new ImageProcessor(filePath);
         this.chi = new ChiSquareTest();
-        this.X = new long[64];
-        
+        this.XR = new long[64];
+        this.XG = new long[64];
+        this.XB = new long[64];
+        this.YR = new double[64];
+        this.YG = new double[64];
+        this.YB = new double[64];
+        this.ZR = new double[64];
+        this.ZG = new double[64];
+        this.ZB = new double[64];
     }
 }
